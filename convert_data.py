60dbc3d408952be6a7fc52b8260e4dfb552b6c70
monil2043 <121935207+monil2043@users.noreply.github.com>
2024-03-13 16:57:38 +0530

    Update convert_data.py

--- convert_data.py
+++ convert_data.py
@@ -1,44 +1,32 @@
 import json
 
-def filter_empty_values(data):
-    if isinstance(data, dict):
-        filtered_data = {}
-        for key, value in data.items():
-            if isinstance(value, dict):
-                filtered_value = filter_empty_values(value)
-                if filtered_value:  # Only add non-empty dictionaries
-                    filtered_data[key] = filtered_value
-            elif value and not value.isspace():  # Check if value is not null or empty
-                filtered_data[key] = value
-        return filtered_data
-    else:
-        return data  # Return data unchanged if it's not a dictionary
+def transform_item(item):
+    dynamodb_item = {}
+    for key, value in item.items():
+        if isinstance(value, dict) and 'S' in value:
+            dynamodb_item[key] = value['S']
+        elif isinstance(value, dict) and 'M' in value:
+            sub_item = transform_item(value['M'])
+            dynamodb_item[key] = sub_item
+    return dynamodb_item
 
 def convert_to_dynamodb_format(data):
     dynamodb_data = []
-    for item in data:
-        filtered_item = filter_empty_values(item)
-        dynamodb_item = {
-            "PutRequest": {
-                "Item": {
-                    key: {"S": str(value)}  # Convert all values to strings
-                    for key, value in filtered_item.items()
-                }
+    for item in data['Items']:
+        dynamodb_item = transform_item(item)
+        dynamodb_data.append({
+            'PutRequest': {
+                'Item': dynamodb_item
             }
-        }
-        dynamodb_data.append(dynamodb_item)
+        })
     return dynamodb_data
 
 # Load input data from JSON file
 with open('data.json') as f:
     input_data = json.load(f)
 
-# Ensure input data is a list of dictionaries
-if isinstance(input_data, list):
-    # Convert data to DynamoDB format
-    dynamodb_data = convert_to_dynamodb_format(input_data)
+# Convert data to DynamoDB format
+dynamodb_data = convert_to_dynamodb_format(input_data)
 
-    # Print the DynamoDB formatted data
-    print(json.dumps({"migration-table": dynamodb_data}, indent=4))
-else:
-    print("Input data must be a list of dictionaries.")
+# Print the DynamoDB formatted data
+print(json.dumps({'migration-table': dynamodb_data}, indent=4))
