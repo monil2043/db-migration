f19fac10c89ad8ff99e0d57d60c7219976171ff4
Monil <monil.ranka@gmail.com>
2024-03-13 20:52:39 +0530

    basic_exec

--- upload_to_dynamodb.py
+++ upload_to_dynamodb.py
@@ -8,7 +8,7 @@ def upload_to_dynamodb(json_file_path, table_name, region_name):
     with open(json_file_path) as f:
         data = json.load(f)
     
-    for item in data['mig-dev03-connect-p-ssp-extensions']:
+    for item in data['migration-table']:
         try:
             table.put_item(Item=item['PutRequest']['Item'])
             print(f"Uploaded item: {item['PutRequest']['Item']}")
@@ -16,8 +16,8 @@ def upload_to_dynamodb(json_file_path, table_name, region_name):
             print(f"Failed to upload item: {item['PutRequest']['Item']}, Error: {e}")
 
 if __name__ == "__main__":
-    json_file_path = "write_data.json"  # Replace with the path to your JSON file
-    table_name = "mig-dev03-connect-p-ssp-extensions"  # Replace with your DynamoDB table name
+    json_file_path = "basic_table.json"  # Replace with the path to your JSON file
+    table_name = "migration-table"  # Replace with your DynamoDB table name
     region_name = "us-west-2"  # Replace with your desired AWS region
     
     upload_to_dynamodb(json_file_path, table_name, region_name)
