aa0db78c8809f7d942ff4960b6859f81a58e2bef
monil2043 <121935207+monil2043@users.noreply.github.com>
2024-03-13 17:08:03 +0530

    Update convert_data.py

--- convert_data.py
+++ convert_data.py
@@ -3,22 +3,24 @@ import json
 def transform_item(item):
     dynamodb_item = {}
     for key, value in item.items():
-        if isinstance(value, dict) and 'S' in value:
-            dynamodb_item[key] = value['S']
+        if isinstance(value, dict) and 'S' in value and value['S'] != "":
+            dynamodb_item[key] = {'S': value['S']}
         elif isinstance(value, dict) and 'M' in value:
             sub_item = transform_item(value['M'])
-            dynamodb_item[key] = sub_item
+            if sub_item:  # Only add if there are non-empty values
+                dynamodb_item[key] = {'M': sub_item}
     return dynamodb_item
 
 def convert_to_dynamodb_format(data):
     dynamodb_data = []
     for item in data['Items']:
         dynamodb_item = transform_item(item)
-        dynamodb_data.append({
-            'PutRequest': {
-                'Item': dynamodb_item
-            }
-        })
+        if dynamodb_item:  # Only add if there are non-empty values
+            dynamodb_data.append({
+                'PutRequest': {
+                    'Item': dynamodb_item
+                }
+            })
     return dynamodb_data
 
 # Load input data from JSON file
